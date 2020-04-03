48fe4eff2fcee53bac3dc2f7dcc75f36d2f9bca4
Ola Karlsson <olakar@google.com>
2020-04-03 16:33:12 +1100

    Basics are in.

--- cmd/config/verify.go
+++ cmd/config/verify.go
@@ -1,6 +1,9 @@
 package config
 
-import "github.com/spf13/cobra"
+import (
+	"github.com/spf13/cobra"
+	"github.com/spinnaker/spin/cmd/gateclient"
+)
 
 type VerifyOptions struct {
 	*configOptions
@@ -17,14 +20,22 @@ func NewVerifyCmd(options configOptions) *cobra.Command {
 	//	configOptions: &options,
 	//}
 	cmd := &cobra.Command{
+		Aliases: []string{"verify", "ve"},
 		Short:   verifyConfigShort,
 		Long:    verifyConfigLong,
 		Example: verifyConfigExample,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return verifyConfig(cmd, args)
+		},
 	}
 
 	return cmd
 }
 
 func verifyConfig(cmd *cobra.Command, args []string) error {
+	_, err := gateclient.NewGateClient(cmd.InheritedFlags())
+	if err != nil {
+		return err
+	}
 	return nil
 }
